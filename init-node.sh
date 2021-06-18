ebb820ec6dfd60f4f9408944ddaabaeaddaedd87
hchsiao <hchsiao@vlsilab.ee.ncku.edu.tw>
2021-06-18 05:41:35 +0000

    first commit

---
+++ init-node.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# Description: My Linode initialization script.
+#              The node should be created with at
+#              least a public key attached.
+
+USERNAME=hchsiao
+GIT_EMAIL=hchsiao@vlsilab.ee.ncku.edu.tw
+
+# Create user for daily use
+useradd $USERNAME
+mkdir -p /home/$USERNAME
+
+# Copy public key for login
+cp -r $HOME/.ssh /home/$USERNAME
+chown -R $USERNAME:$USERNAME /home/$USERNAME
+
+# Allow sudo without password
+echo "$USERNAME ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+
+# Won't need root anymore
+passwd -d root
+
+# Setup editor
+apt install -y ctags
+su $USERNAME -c "git clone --recursive https://github.com/hchsiao/vim-env.git /home/$USERNAME/.vim"
+
+# Setup shell
+apt-add-repository -y ppa:fish-shell/release-3
+apt-get update
+apt-get install -y fish
+chsh hchsiao -s $(grep "fish" /etc/shells)
+
+# Setup git
+su $USERNAME -c "git config --global user.email $GIT_EMAIL"
+su $USERNAME -c "git config --global user.name $USERNAME"
